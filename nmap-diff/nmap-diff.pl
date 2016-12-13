47770bbafeb40e826fce20c908e05595b55119a9
Lee Heath <madhat@unspecific.com>
2016-12-13 10:21:11 -0600

    update found in seperate locaiton.

--- nmap-diff/nmap-diff.pl
+++ nmap-diff/nmap-diff.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/perl
+#---------------------------------------
+#
+#   Writen by MadHat (madhat@unspecific.com)
+# http://www.unspecific.com/nmap/diff/
+#
 # Copyright (c) 2001-2002, MadHat (madhat@unspecific.com)
 # All rights reserved.
 #
@@ -11,7 +16,7 @@
 #   * Redistributions in binary form must reproduce the above copyright
 #     notice, this list of conditions and the following disclaimer in
 #     the documentation and/or other materials provided with the distribution.
-#   * Neither the name of MadHat Productions nor the names of its
+#   * Neither the name of Unspecific Consulting nor the names of its
 #     contributors may be used to endorse or promote products derived
 #     from this software without specific prior written permission.
 #
@@ -26,42 +31,108 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-use POSIX "strftime";
-use Getopt::Std;
-getopts("vd:s:i:b:");
-
-# -s for subnet
-# -i for IP, faster if used with -s
-# -d debug
-# -v verbose (tells removed ports as well)
-# -b for the number of days back to go for the base
-#    this will still compair to the latest scan
+#
+#---------------------------------------
 
 # where the nmap logs are stored
-$logdir = "/home/ysec/logs";
+my $logdir = "/usr/local/var/log/nmap";
+# sendmail
+my $sendmail = "/usr/sbin/sendmail";
 
+#---------------------------------------
+# Don't change anything below here
+#---------------------------------------
+$VERSION = '1.3';
 ##############################################################
 #
 #
-$sdate = time - 86400;
-if ($opt_b > 2) {
-  $bdate = time - $opt_b * 86400;
-} else {
-  $bdate = time - 172800;
+use POSIX "strftime";
+use Getopt::Std;
+getopts("hvd:s:i:b:l:m:");
+#
+if ($opt_h) {
+  &usage;
 }
-$basedate = strftime "%m%d%Y", localtime $bdate;
-$scandate = strftime "%m%d%Y", localtime $sdate;
-print "Compairing $basedate to $scandate\n" if ($opt_d);
+if ($opt_l) {
+  $logdir = $opt_l;
+}
+#
+#my $html = 0;
+#
+#
+if (defined $ENV{'REQUEST_METHOD'}) {
+  $html = 1;
+  use CGI ":standard";
+  use CGI::Carp "fatalsToBrowser";
+  print header, start_html('NMAP Diff');
+  if (param('d') =~ /^(\d)$/) {
+    $opt_d = $1;
+  } 
+  $opt_v = 1;
+  print "<form method=get>\n";
 
-opendir(DIR, $logdir);
+  if (param('bdate') =~ /^\d{8}$/ and param('bdate') > 20081202) {
+    $basedate = param('bdate');
+  } else {
+    $bdate = time - 86400; # - 172800;
+    $basedate = strftime "%Y%m%d", localtime $bdate;
+  }
+  print "<input name='bdate' value='$basedate' size='8'> Base Date (Initial Scan YYYYMMDD)<br>\n";
+  if (param('sdate') =~ /^\d{8}$/ and param('sdate') > 20081202) {
+    $scandate = param('sdate');
+  } else {
+    $sdate = time;
+    $scandate = strftime "%Y%m%d", localtime $sdate;
+  }
+  print "<input name='sdate' value='$scandate' size='8'> Scan Date (Compair Scan YYYYMMDD)<br>\n";
+  if ($sdate < $bdate) {
+    die("Base Date can NOT be BEFORE the Scan Date");
+  }
+  if (
+      param('ip') =~ /^(\d{1,3}\.\d{1,3})$/ or
+      param('ip') =~ /^(\d{1,3}\.\d{1,3}\.\d{1,3})$/
+    ) {
+    $ip = $1;
+    $opt_s = $ip;
+  } elsif (
+      param('ip') =~ /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$/
+    ) {
+    $ip = $1;
+    $opt_i = $ip;
+  }
+  print "<input name=ip value='$ip' size=14> Search IP (Partial IP Works XXX.XXX)<br>\n";
+  print "<input type=submit value='Show Diff'>\n";
+
+  print "<pre>\n";
+} else {
+  $sdate = time; # - 86400;
+  if ($opt_b > 2) {
+    $bdate = time - $opt_b * 86400;
+  } else {
+    $bdate = time - 86400; # - 172800;
+  }
+  $basedate = strftime "%Y%m%d", localtime $bdate;
+  $scandate = strftime "%Y%m%d", localtime $sdate;
+}
+if ($opt_i =~ /^(\d{1,3}\.\d{1,3}\.\d{1,3})\.\d{1,3}/) {
+  $opt_s = $1;
+}
+print "Compairing $basedate to $scandate\n" if ($opt_v or $opt_d);
+
+opendir(DIR, $logdir) or die "ERROR: Unable to open $logdir: $!\n";
 @dir = readdir(DIR);
 close(DIR);
 
+if ($opt_m) {
+  open(STDOUT,"| $sendmail -t") or die "Unable to open sendmail";
+  print "To: $opt_m\n";
+  print "Subject: Port Changes from $basedate to $scandate\n\n";
+}
 FILE: for $file (@dir) {
   next FILE if ($file =~ /^\./);
   next FILE if ($opt_s and $file !~ /$opt_s/);
-  if ($file =~ /^$basedate(\.\d{1,3}\.\d{1,3}\.\d{1,3}\.nmap)$/) {
+  print "examining $file\n" if ($opt_d > 3);
+  if ($file =~ /^$basedate(\.\d{1,3}\.\d{1,3}\.\d{1,3}\.gnmap)$/) {
     $exten = $1;
     print "compairing $basedate$exten to $scandate$exten\n" if ($opt_d);
     open (BASE, "$logdir/$basedate$exten");
@@ -118,15 +189,13 @@ FILE: for $file (@dir) {
                 and $state eq 'open' 
               ) {
               print "Filtered -> Open $ip:$port\n" if ($opt_d > 1);
-              $data .= sprintf("  +%5u/tcp   open   %s\n", $port, $name)
-                unless (($port == 80 or $port == 443 ) and !$opt_v);
+              $data .= sprintf("  +%5u/tcp   open   %s\n", $port, $name);
             } elsif (
                 $base_port{$port} eq 'closed' 
                 and $state eq 'open'
               ) {
               print "Closed -> Open $ip:$port\n" if ($opt_d > 1);
-              $data .= sprintf("  +%5u/tcp   open   %s\n", $port, $name)
-                unless (($port == 80 or $port == 443 ) and !$opt_v);
+              $data .= sprintf("  +%5u/tcp   open   %s\n", $port, $name);
             } elsif (
                 $base_port{$port} eq 'open' 
                 and $state eq 'filtered'
@@ -140,14 +209,14 @@ FILE: for $file (@dir) {
               ) {
               print "GONE $ip:$port\n" if ($opt_d > 1);
               $data .= sprintf("  -%5u/tcp   open   %s\n", $port, $name)
-                unless ( ($port == 80 or $port == 443) and !$opt_v);
+                unless (!$opt_v);
             } elsif (
                 !$base_port{$port}
                 and $state eq 'open'
               ) {
               print "Opened from N/A $ip:$port\n" if ($opt_d > 1);
               $data .= sprintf("  +%5u/tcp   open   %s\n", $port, $name)
-                unless ( ($port == 80 or $port == 443) and !$opt_v);
+                unless (!$opt_v);
             } elsif (
                 $base_port{$port} eq 'open' 
                 and $state eq 'closed'
@@ -178,21 +247,49 @@ FILE: for $file (@dir) {
             }
           }
         }
-        if (
-          $data =~ /\s[\-\+]\s/ and 
-          ( 
-            $data !~ m<^\s{2}\+\s{3}25/tcp\s{3}open\s{3}smtp\n$>sx 
-            and $dns !~ /\.mail\./
-          )
-        ) {
-          print "$ip $dns\n";
-          print "$data\n";
-          print "-" x 70 . "\n$port_info\n$bport_info\n" if ($opt_d > 2);
-        }
+	if ( $data =~ /\s[\-\+]\s/ ) {
+	  print "$ip $dns\n";
+	  print "$data\n";
+	  print "-" x 70 . "\n$port_info\n$bport_info\n" if ($opt_d > 2);
+	}
         print "-" x 70 . "\n" if ($opt_d);
-      } 
+      }
     }
   } else {
     next FILE;
   }
+}
+if ($opt_m) {
+  close (STDOUT);
+}
+1;
+
+
+sub usage {
+  print " : nmap-diff - v$VERSION - MadHat (at) Unspecific.com\n";
+  print " : http://www.unspecific.com/nmap/diff/\n\n";
+
+  print <<_EOF_;
+    nmap-diff is designed to be used with the log files
+      generated from the nmap-wrapper
+
+$0 [-hv] [-s <subnet>] [-i <ip>] [-b <days>]  \
+          [-m <email>] [-l <logdir>]
+
+  -h help (this stuff)
+  -v is for verbose.  This will add all changed ports.  
+     Default is to only who new open ports
+  -s <subnet> shows only thaing in that subnet.  
+     At this time the subnet accepted is a class C only.
+  -i <IP> only reports on that specific IP.
+  -b <days> sets the base to <days> days back and compares 
+     to yesterday's scan.  so -b 7 will compare the current 
+     scan to the scan from 1 week ago
+  -l <logdir> to specify where the log directory
+     This can be hard coded by editing the script
+  -m <email> to email the output to <email> when the report 
+     is generated
+
+_EOF_
+  exit 1;
 }
